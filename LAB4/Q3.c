9308e161cd755aea6b73c7940e97aa133940afda
yanyan-alien <63704838+yanyan-alien@users.noreply.github.com>
2020-10-28 15:02:01 +0800

    Create Q3.c

---
+++ LAB4/Q3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#define INIT_VALUE 1000
+int palindrome(char *str);
+int main()
+{
+   char str[80], *p;
+   int result = INIT_VALUE;
+
+   printf("Enter a string: \n");
+   fgets(str, 80, stdin);
+   if (p=strchr(str,'\n')) *p = '\0';
+   result = palindrome(str);
+   if (result == 1)
+      printf("palindrome(): A palindrome\n");
+   else if (result == 0)
+      printf("palindrome(): Not a palindrome\n");
+   else
+      printf("An error\n");
+   return 0;
+}
+int palindrome(char *str)
+{
+   /* Write your code here */
+   int length = strlen(str), middle, i;
+   middle = length / 2;
+   for(i=0;i<=middle;i++)if(str[i]!=str[length-1-i])return 0;
+   return 1;
+}
+
