8ab1eb9cb3348e6ae33d94f8719a1284faeec957
yanyan-alien <63704838+yanyan-alien@users.noreply.github.com>
2020-11-10 19:12:11 +0800

    Create processString.c

---
+++ Character Strings/processString.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+void processString(char *str, int *totVowels, int *totDigits);
+int main()
+{
+char str[50], *p;
+int totVowels, totDigits;
+printf("Enter the string: \n");
+fgets(str, 80, stdin);
+if (p=strchr(str,'\n')) *p = '\0';
+processString(str, &totVowels, &totDigits);
+printf("Total vowels = %d\n", totVowels);
+printf("Total digits = %d\n", totDigits);
+return 0;
+}
+
+
+void processString(char *str, int *totVowels, int *totDigits)
+{
+ /* Write your program code here */
+ int i=0;
+ *totVowels = 0; *totDigits = 0;
+ while(str[i] != '\0')
+ {
+     if(str[i]=='a'|str[i]=='e'|str[i]=='i'|str[i]=='o'|str[i]=='u')++*totVowels;
+     if(isdigit(str[i]))++*totDigits;
+     ++i;
+ }
+}
