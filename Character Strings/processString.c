f971efc399953dd0936dbdfbaae83ba0eca6fd95
yanyan-alien <63704838+yanyan-alien@users.noreply.github.com>
2020-11-11 17:03:47 +0800

    Update processString.c

--- Character Strings/processString.c
+++ Character Strings/processString.c
@@ -4,15 +4,15 @@
 void processString(char *str, int *totVowels, int *totDigits);
 int main()
 {
-char str[50], *p;
-int totVowels, totDigits;
-printf("Enter the string: \n");
-fgets(str, 80, stdin);
-if (p=strchr(str,'\n')) *p = '\0';
-processString(str, &totVowels, &totDigits);
-printf("Total vowels = %d\n", totVowels);
-printf("Total digits = %d\n", totDigits);
-return 0;
+ char str[50], *p;
+ int totVowels, totDigits;
+ printf("Enter the string: \n");
+ fgets(str, 80, stdin);
+ if (p=strchr(str,'\n')) *p = '\0';
+ processString(str, &totVowels, &totDigits);
+ printf("Total vowels = %d\n", totVowels);
+ printf("Total digits = %d\n", totDigits);
+ return 0;
 }
 
 
@@ -23,8 +23,9 @@ void processString(char *str, int *totVowels, int *totDigits)
  *totVowels = 0; *totDigits = 0;
  while(str[i] != '\0')
  {
-     if(str[i]=='a'|str[i]=='e'|str[i]=='i'|str[i]=='o'|str[i]=='u')++*totVowels;
-     if(isdigit(str[i]))++*totDigits;
-     ++i;
+  if(str[i]=='a'|str[i]=='e'|str[i]=='i'|str[i]=='o'|str[i]=='u'|
+  str[i]=='A'|str[i]=='E'|str[i]=='I'|str[i]=='O'|str[i]=='U') ++(*totVowels);
+  if(isdigit(str[i])) ++(*totDigits);
+  ++i;
  }
 }
